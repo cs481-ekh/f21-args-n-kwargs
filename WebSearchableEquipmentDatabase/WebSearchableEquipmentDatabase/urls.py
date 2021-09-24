b0185ccaa144ff99d7212b8ecce461e3ae920586
ianrwhitney <51089822+ianrwhitney@users.noreply.github.com>
2021-09-23 21:33:37 -0600

    Closes #16 (#17)

--- WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/urls.py
+++ WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from accounts import views as auth_views
-
 urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
+    path('', include('accounts.urls'))
+    # path('login/', auth_views.login, name='login'),
+    # path('logout/', auth_views.logout, name='logout'),
 ]
