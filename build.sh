5140207619365e3514591833ff00d7c00e16c131
ianrwhitney <51089822+ianrwhitney@users.noreply.github.com>
2021-09-24 13:48:22 -0600

    Closes #26 (#25)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python -m pip install --upgrade pip
-pip install pylint
-cd WebSearchableEquipmentDatabase || exit 1
-pylint --fail-under=9 $(ls -R|grep .py$|xargs)
+python -m pip install --upgrade pip || exit $?
+pip install pylint || exit $?
+cd WebSearchableEquipmentDatabase || exit $?
+pylint --fail-under=9 $(ls -R|grep .py$|xargs) || exit $?
