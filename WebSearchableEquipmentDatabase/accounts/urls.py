7a1c1755d97065c4ca3346d21c299376ee12fd6e
ianrwhitney <51089822+ianrwhitney@users.noreply.github.com>
2021-10-02 13:18:03 -0600

    Closes #7 (#41)

--- WebSearchableEquipmentDatabase/accounts/urls.py
+++ WebSearchableEquipmentDatabase/accounts/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+
+
+    #Password Reset Paths as_view() take a template as an argument to customized view
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
 ]
