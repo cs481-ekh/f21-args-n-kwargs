058865d1e5c231b59bfcb4fff1412e7827d65ec8
Ian Whitney <ian.r.whitney@gmail.com>
2021-10-02 13:15:03 -0600

    password recovery mechanism implemented

--- WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/settings.py
+++ WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/settings.py
@@ -131,3 +131,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '<your gmail here>'
+EMAIL_HOST_PASSWORD = '<password here>'
