2c752fc0aa0c6cbe298f16a1be45f25ad41255e6
ianrwhitney <51089822+ianrwhitney@users.noreply.github.com>
2021-10-02 13:12:40 -0600

    Closes #5 (#40)

--- WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/settings.py
+++ WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,8 +27,10 @@ DEBUG = True  # TODO: change to false once in production
 
 ALLOWED_HOSTS = []  # TODO: will need to be the url our website is hosted at once in production
 
-
 # Application definition
+
+# points the user model at our custom model Accounts versus default Django User model
+AUTH_USER_MODEL = 'accounts.Account'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -77,7 +78,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'WebSearchableEquipmentDatabase.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -87,7 +87,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -107,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -120,7 +118,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- WebSearchableEquipmentDatabase/accounts/models.py
+++ WebSearchableEquipmentDatabase/accounts/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-# Create your models here.
+
+class AccountManager(BaseUserManager):
+    """Account Manager is the interface that we interact with our Accounts when interacting with the db account"""
+
+    def create_user(self, email, password=None):
+        """create_user creates a user account with email and password saving it to the Accounts table
+            if email is not specified it will block with a ValueErrror"""
+        if not email:
+            raise ValueError("Email cannot be empty")
+
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """create_superuser creates a superuser with email and password and sets accounts permissions all to true"""
+        user = self.create_user(email=self.normalize_email(email), password=password)
+
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    """Account model that will be stored in db that extends abstractBaseuser and requires email and password
+        username will be the users email and will be saved to the Accounts table with default permissions"""
+    email = models.EmailField(verbose_name="email", max_length=254, unique=True)
+
+    # to track user status level
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    # what we want the user to login in with
+    USERNAME_FIELD = 'email'
+
+    # interface we use to interace with this model with the db
+    objects = AccountManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        ordering = ["email"]  # how the rows will be ordered
+        db_table = "Accounts"
