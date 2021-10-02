22d4b83f77d1cb781f7fdf333bee26d57a6e6e89
ianrwhitney <51089822+ianrwhitney@users.noreply.github.com>
2021-10-02 13:24:36 -0600

    Closes #4 (#43)

--- WebSearchableEquipmentDatabase/accounts/admin.py
+++ WebSearchableEquipmentDatabase/accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.models import Account
+
+
+class AccountAdmin(UserAdmin):
+    """Customizes the admin page to display desired information regarding Accounts also sets up filtering ability
+        for site admin"""
+    model = Account
+    list_display = ('email', 'is_staff', 'is_active', 'is_superuser')
+    list_filter = ('email', 'groups', 'is_staff', 'is_active', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups')}),
+    # for finer grain control add - , 'user_permissions'
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+# registers our model and admin
+admin.site.register(Account, AccountAdmin)
