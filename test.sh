b65b06a42f60263f223ab49628ef6ec4c17d4444
Ian Whitney <ian.r.whitney@gmail.com>
2021-10-02 13:08:35 -0600

    updated test script to makemigrations for custom model

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 python -m pip install --upgrade pip || exit $?
 pip install -r requirements.txt || exit $?
 cd WebSearchableEquipmentDatabase || exit $?
+python manage.py makemigrations || exit $?
 python manage.py test || exit $?
