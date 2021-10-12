1c937be58f5af2bcf37cc5f5f650f28293abbaf7
Nick Stolarow <nickstolarow@u.boisestate.edu>
2021-10-12 14:11:16 -0600

    fixes

--- WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/urls.py
+++ WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/urls.py
@@ -16,26 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from accounts import views as auth_views
-from django.contrib.auth import views as django_auth_views
 
 urlpatterns = [
     path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('login/', auth_views.login, name='login'),
-    path('logout/', auth_views.logout, name='logout'),
-    path('reset_password/', django_auth_views.PasswordResetView.as_view(template_name='accounts/reset_password.html'),
-         name='reset_password'),
-    path('reset_password_sent/',
-         django_auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),
-         name="password_reset_done"),
-    path('reset/<uidb64>/<token>/',
-         django_auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),
-         name="password_reset_confirm"),
-    path('reset_password_complete/',
-         django_auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
-         name="password_reset_complete"),
-    path('register/', auth_views.register, name='register'),
     # This will put every link in the equipment.urls.py file under equipment/{methodName}
     path('equipment/', include('equipment.urls'))
 
