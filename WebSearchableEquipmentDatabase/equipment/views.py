eacf6dbb2d138de6dce462aa9b984d2112ad785b
Nick Stolarow <nickstolarow@u.boisestate.edu>
2021-10-17 00:34:31 -0600

    improvements to file upload page

--- WebSearchableEquipmentDatabase/equipment/views.py
+++ WebSearchableEquipmentDatabase/equipment/views.py
@@ -12,16 +12,21 @@ def dataTable(request):
 
 
 def upload_csv(request):
+    error = False
     if not request.user.is_superuser:
         return redirect('home')
     if request.POST:
         file = request.POST.get('file', False)
         if not file:
             messages.error(request, "Please select a file to be uploaded.")
-        if not str(file).lower().endswith('.csv'):
+            error = True
+        elif not str(file).lower().endswith('.csv'):
             messages.error(request, "File must be a .csv")
+            error = True
 
         # TODO: actual parsing logic
+        if not error:
+            messages.success(request, "File uploaded successfully!")
 
     # Used to dynamically set the nav bar link to active.
     # Example: <a class="nav-item nav-link {% if upload_csv %}active{% endif %}">Upload File</a>
