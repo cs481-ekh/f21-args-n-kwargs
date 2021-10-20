3ca153bf95c559b1e7a1ee72277b818c2c94e052
ianrwhitney <51089822+ianrwhitney@users.noreply.github.com>
2021-10-20 13:40:34 -0600

    Implements #58 (#68)

--- WebSearchableEquipmentDatabase/equipment/admin.py
+++ WebSearchableEquipmentDatabase/equipment/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+# from equipment.models import Category, Equipment
+
+from equipment.models import Equipment
+# class CategoryInline(admin.StackedInline):
+#     """Creates a class that allows the admin to change category inline with equipment"""
+#     model = Category
+#
+#
+# class EquipmentAdmin(admin.ModelAdmin):
+#     """Uses the inline category class to improve ease of use by admin to edit category with same interface
+#         as the equipment table"""
+#     inlines = [
+#         CategoryInline,
+#     ]
+#
+#
+# # registers the equipment model along with the backend admin to manipulate the  model
+admin.site.register(Equipment)
