02ab5cc1f7362016ccd59578bcdc6f6fdd1bd127
Ian Whitney <ian.r.whitney@gmail.com>
2021-10-20 13:13:38 -0600

    Implements equipment model

--- WebSearchableEquipmentDatabase/equipment/models.py
+++ WebSearchableEquipmentDatabase/equipment/models.py
@@ -1,3 +1,34 @@
+import datetime
+
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+def max_value_current_year(value):
+    """small helper function to set max year allowed for equipment also together
+        allow validators on year field to be callable"""
+    return 3000
+
 
 # Create your models here.
+class Equipment(models.Model):
+    """Equipment Model with associated fields to describe a new piece of equipment"""
+    Permission = (
+        ("Student", "Student"),
+        ("Faculty", "Faculty"),
+        ("Guest", "Guest"),
+    )
+    name = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    manufacturer = models.CharField(max_length=255)
+    year = models.IntegerField('year', validators=[MinValueValidator(1950), max_value_current_year])
+    pi = models.CharField(max_length=255)
+    contact = models.TextField(blank=True, null=True)
+    room = models.CharField(max_length=255, blank=True, null=True)
+    lab = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    url = models.TextField(blank=True, null=True)
+    permission = models.CharField(max_length=255, choices=Permission)
+
+    def __str__(self):
+        return f'{self.name}, {self.model}'
---
+++ WebSearchableEquipmentDatabase/equipment/tests/test_models.py
@@ -0,0 +1,77 @@
+from django.test import TestCase
+from equipment.models import Equipment #Category
+
+
+class EquipmentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Equipment.objects.create(name="Rotary Disc cutter",
+                                 model="Model 360",
+                                 manufacturer="South Bay",
+                                 year=2016,
+                                 pi="Steve Jobs",
+                                 contact="Steve Jobs",
+                                 room="CS1001",
+                                 lab="BSCMC",
+                                 description="Cutting 3 mm",
+                                 url="www.google.com",
+                                 permission="Student")
+
+    def test_equipment_name(self):
+        equip = Equipment.objects.get(id=1)
+        name_label = equip.name
+        self.assertEqual(name_label, "Rotary Disc cutter")
+
+    def test_equipment_model(self):
+        equip = Equipment.objects.get(id=1)
+        model_label = equip.model
+        self.assertEqual(model_label, "Model 360")
+
+    def test_equipment_year(self):
+        equip = Equipment.objects.get(id=1)
+        year_label = equip.year
+        self.assertEqual(year_label, 2016)
+
+    def test_equipment_pi(self):
+        equip = Equipment.objects.get(id=1)
+        pi_label = equip.pi
+        self.assertEqual(pi_label, "Steve Jobs")
+
+    def test_equipment_contact(self):
+        equip = Equipment.objects.get(id=1)
+        contact_label = equip.contact
+        self.assertEqual(contact_label, "Steve Jobs")
+
+    def test_equipment_room(self):
+        equip = Equipment.objects.get(id=1)
+        room_label = equip.room
+        self.assertEqual(room_label, "CS1001")
+
+    def test_equipment_lab(self):
+        equip = Equipment.objects.get(id=1)
+        lab_label = equip.lab
+        self.assertEqual(lab_label, "BSCMC")
+
+    def test_equipment_description(self):
+        equip = Equipment.objects.get(id=1)
+        description_label = equip.description
+        self.assertEqual(description_label, "Cutting 3 mm")
+
+    def test_equipment_url(self):
+        equip = Equipment.objects.get(id=1)
+        url_label = equip.url
+        self.assertEqual(url_label, "www.google.com")
+
+    def test_equipment_to_string(self):
+        equip = Equipment.objects.get(id=1)
+        self.assertEqual(equip.__str__(), "Rotary Disc cutter, Model 360")
+
+    def test_equipment_permission(self):
+        equip = Equipment.objects.get(id=1)
+        permission_label = equip.permission
+        self.assertEqual(permission_label, "Student")
+
+
+
+
