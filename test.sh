c3a7f356a814b973ebcaf1d6d91ed5447e0e6011
Ian Whitney <ian.r.whitney@gmail.com>
2021-10-20 13:28:41 -0600

    updated test script with test export

--- test.sh
+++ test.sh
@@ -3,4 +3,5 @@ python -m pip install --upgrade pip || exit $?
 pip install -r requirements.txt || exit $?
 cd WebSearchableEquipmentDatabase || exit $?
 python manage.py makemigrations || exit $?
+import experiment.export.tests || exit $?
 python manage.py test || exit $?
