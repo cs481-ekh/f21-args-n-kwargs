df5b908cc534c7211f69ffb5a56f2c031ac6942d
Ian Whitney <ian.r.whitney@gmail.com>
2021-10-20 13:52:55 -0600

    updated custom admin backend

--- WebSearchableEquipmentDatabase/equipment/admin.py
+++ WebSearchableEquipmentDatabase/equipment/admin.py
@@ -3,6 +3,18 @@ from django.contrib import admin
 from equipment.models import Category, Equipment
 
 
+class CategoryInline(admin.StackedInline):
+    """Creates a class that allows the admin to change category inline with equipment"""
+    model = Category
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    """Uses the inline category class to improve ease of use by admin to edit category with same interface
+        as the equipment table"""
+    inlines = [
+        CategoryInline,
+    ]
+
+
 # registers the equipment model along with the backend admin to manipulate the  model
-admin.site.register(Equipment)
-admin.site.register(Category)
+admin.site.register(Equipment, EquipmentAdmin)
