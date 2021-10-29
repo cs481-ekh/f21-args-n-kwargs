25aac6bd417e73873647b0c5948f0831c6af82be
Ian Whitney <ian.r.whitney@gmail.com>
2021-10-29 12:42:20 -0600

    refactored endpoints to allow ease of navigation

--- WebSearchableEquipmentDatabase/equipment/urls.py
+++ WebSearchableEquipmentDatabase/equipment/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('base/', views.index),
-    path('datatable/', views.data_table, name='home'),
+    path('', views.data_table, name='home'),
     path('uploadcsv/', views.upload_csv, name='uploadCSV'),
+    path('testing', views.testing, name='testing'),
 ]
