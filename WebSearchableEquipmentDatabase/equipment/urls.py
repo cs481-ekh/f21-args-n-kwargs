e0e9c211aa51bd3fd1957b5a397f72df8a5d9adf
Ian Whitney <ian.r.whitney@gmail.com>
2021-11-02 10:43:57 -0600

    temp commit

--- WebSearchableEquipmentDatabase/equipment/urls.py
+++ WebSearchableEquipmentDatabase/equipment/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.data_table, name='home'),
     path('uploadcsv/', views.upload_csv, name='uploadCSV'),
     path('testing', views.testing, name='testing'),
+    path('ajax/crud/delete/', views.DeleteEquipm.as_view(), name='crud_ajax_delete'),
 ]
