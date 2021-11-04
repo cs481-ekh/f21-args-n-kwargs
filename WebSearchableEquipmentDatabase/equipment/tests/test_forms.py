5b763da833a0e706df1acf2979ae226b306e6852
Nick Stolarow <59889420+nickStolarow@users.noreply.github.com>
2021-11-03 22:36:50 -0600

    53 (#102)
    * happy path working - needs better error messages when parsing fails
    
    * file upload fully working
    
    * fix
    
    * fix

--- WebSearchableEquipmentDatabase/equipment/tests/test_forms.py
+++ WebSearchableEquipmentDatabase/equipment/tests/test_forms.py
@@ -16,28 +16,14 @@ class EquipmentFormTest(TestCase):
 
     def test_equipment_form_valid(self):
         form = EquipmentForm(data={'name': self.name, 'model': self.model, 'manufacturer': self.manufacturer,
-                                   'year': 2016, 'pi': self.pi, 'contact': self.contact,
+                                   'year': '2016', 'pi': self.pi, 'contact': self.contact,
                                    'location': self.location,  'description': self.description,
                                    'url': self.url, 'permission': self.permission})
         self.assertTrue(form.is_valid())
 
-    def test_equipment_form_invalid_year_min(self):
-        form = EquipmentForm(data={'name': self.name, 'model': self.model, 'manufacturer': self.manufacturer,
-                                   'year': 1949, 'pi': self.pi, 'contact': self.contact,
-                                   'location': self.location, 'description': self.description,
-                                   'url': self.url, 'permission': self.permission})
-        self.assertFalse(form.is_valid())
-
-    def test_equipment_form_invalid_year_max(self):
-        form = EquipmentForm(data={'name': self.name, 'model': self.model, 'manufacturer': self.manufacturer,
-                                   'year': 3001, 'pi': self.pi, 'contact': self.contact,
-                                   'location': self.location, 'description': self.description,
-                                   'url': self.url, 'permission': self.permission})
-        self.assertFalse(form.is_valid())
-
     def test_equipment_form_valid_null_values(self):
         form = EquipmentForm(data={'name': self.name, 'model': self.model, 'manufacturer': self.manufacturer,
-                                   'year': 2020, 'pi': self.pi, 'contact': None,
+                                   'year': '2020', 'pi': self.pi, 'contact': None,
                                    'location': None, 'description': None,
                                    'url': None, 'permission': self.permission})
         self.assertTrue(form.is_valid())
