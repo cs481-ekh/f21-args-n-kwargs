05bedf69bba4c58a762a9d1991cc1fdc9990597c
ianrwhitney <51089822+ianrwhitney@users.noreply.github.com>
2021-11-12 09:16:36 -0700

    Implements #105 (#106)

--- WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/wsgi.py
+++ WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/wsgi.py
@@ -7,9 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/IANWHITNEY/f21-args-n-kwargs/WebSearchableEquipmentDatabase')
+
+sys.path.append('/home/IANWHITNEY/anaconda3/envs/projectvenv/lib/python3.8/site-packages')
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebSearchableEquipmentDatabase.settings')
 
