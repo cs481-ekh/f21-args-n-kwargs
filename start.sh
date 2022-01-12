331320d19d15a80b3b2631821d354148aa742149
Eric K. Henderson <eric@101.ezip.email>
2022-01-12 13:30:31 -0700

    [start.sh] activate py virtual env before running create cmds

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 python3 -m venv ./venv
+. venv/bin/activate
 pip install -r requirements.txt
 python WebSearchableEquipmentDatabase/manage.py migrate
 python WebSearchableEquipmentDatabase/manage.py runserver
