867efd7a95d2734ef4307914f54e673bf9b16b46
Eric K. Henderson <eric@101.ezip.email>
2022-01-25 22:37:33 -0700

    [settings] restore static_root

--- WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/settings.py
+++ WebSearchableEquipmentDatabase/WebSearchableEquipmentDatabase/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT is used in deployment using the manage.py collectstatic command
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
